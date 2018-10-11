6afaa91bedb0df36c8ee6c1d3ee1c06f7224c473
shubhamsrstv <shubham.srivastava9@tcs.com>
2018-10-11 01:56:15 -0700

    added for loop

--- src/main/java/com/example/activemqTopic/ActivemqTopicApplication.java
+++ src/main/java/com/example/activemqTopic/ActivemqTopicApplication.java
@@ -22,8 +22,18 @@ public class ActivemqTopicApplication
 	public static void main(String[] args) throws URISyntaxException, Exception 
 	{
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of messages to send : ");
+		int countSend = sc.nextInt();
+		sc.nextLine();
 		System.out.println("Enter number of consumers : ");
-		int count = sc.nextInt();
+		int countConsume = sc.nextInt();
+		sc.nextLine();
+		System.out.println("Enter the message(s) to send : ");
+		String[] payload = new String[countSend];
+		for(int i=0;i<countSend;i++)
+		{
+			payload[i] = sc.nextLine();
+		}
 		sc.close();
 		Connection connection = null;
 		try 
@@ -32,19 +42,21 @@ public class ActivemqTopicApplication
 			connection = connectionFactory.createConnection();
 			Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
 			Topic topic = session.createTopic("Testing");   
-			for(int i=0;i<count;i++)
+			for(int i=0;i<countConsume;i++)
 			{
 				MessageConsumer consumer = session.createConsumer(topic);
 				consumer.setMessageListener(new ConsumerMessageListener("consumer"+(i+1)));
 			}
 			connection.start();    
+			for(int i=0;i<countSend;i++)
+			{
+				Message msg = session.createTextMessage(payload[i]);
+				MessageProducer producer = session.createProducer(topic);
+				System.out.println("Sending text '" + payload[i] + "'");
+				producer.send(msg);
+				Thread.sleep(1000);
+			}
 			
-			String payload = "Shubham Srivastava is great!!!";
-			Message msg = session.createTextMessage(payload);
-			MessageProducer producer = session.createProducer(topic);
-			System.out.println("Sending text '" + payload + "'");
-			producer.send(msg);
-			Thread.sleep(1000);
 			session.close();
 		}
 		finally
